f391f5fa689a4b0b239b92109976589f7f3cb4bc
Satvik1769 <manchandasatvik@gmail.com>
2023-11-23 08:23:35 +0530

    file upload

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,59 +1,43 @@
-const fs = require("fs");
-const { ThirdwebStorage } = require("@thirdweb-dev/storage");
-const multer = require("multer");
+import { NextResponse } from "next/server";
+import { ThirdwebStorage } from "@thirdweb-dev/storage";
+import multer from "multer";
+
 const storage = new ThirdwebStorage({
   secretKey: process.env.SECRET_KEY,
 });
-
-// (async () => {
-//   const upload = await storage.upload(fs.readFileSync("test.jpg"));
-//   console.log(`Gateway URL - ${storage.resolveScheme(upload)}`);
-// })();
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
+
 const upload = multer({ dest: "uploads/" });
 const uploadMiddleware = upload.single("file");
 
-// async function handler(req, res) {
-//   uploadMiddleware
-//     (req,res,async function(err)){
-//       if(err){
-//         console.error("Error uploading file:", err);
-//       return res.status(500).json({ error: "Error uploading file" });
-//       }
-//   }
-//   console.log(req.file);
-//   const imagePath = req.file.path;
-//   console.log(imagePath);
-//   const upload = await storage.upload(fs.readFileSync(imagePath));
-//   console.log(`Gateway URL - ${storage.resolveScheme(upload)}`);
-// }
-
 async function handler(req, res) {
-  // Use the multer middleware
-  uploadMiddleware(req, res, async function (err) {
+  return uploadMiddleware(req, res, async function (err) {
+    console.log("1");
     if (err) {
+      console.log("2");
       console.error("Error uploading file:", err);
-      return new Response({ error: "Error" });
+      return new NextResponse({ error: "Error uploading file" });
     }
 
     // Now req.file should be populated with information about the uploaded file
-    console.log(req.file);
-    const imagePath = req.file;
+    console.log(req.body);
+    // const imagePath = req.file.path;
 
     try {
       // const uploadResult = await storage.upload(fs.readFileSync(imagePath));
       // console.log(`Gateway URL - ${storage.resolveScheme(uploadResult)}`);
-      return new Response({ message: "Image uploaded successfully" });
+      console.log("3");
+      return new NextResponse({ message: "Image uploaded successfully" });
     } catch (error) {
+      console.log("4");
       console.error("Error uploading image:", error);
-      return new Response({ error: "Error uploading image" });
+      return new NextResponse({ error: "Error uploading image" });
     }
   });
 }
-
 export { handler as POST };
